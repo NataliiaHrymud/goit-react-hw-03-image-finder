8b537bbaf16ff1f5e6bb1440c0de23ff9e784dba
Nataliia Hrymud <ngrimud@gmail.com>
2020-08-19 08:17:10 +0300

    deleted div before li

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -3,22 +3,20 @@ import style from './iImageGalleryItem.module.css';
 import PropTypes from 'prop-types';
 
 const ImageGalleryItem = ({img, id, show}) => {
-    return (
-        <div>
-            <li 
-            className={style.ImageGalleryItem}
-            key={id}
-            onClick={()=> show(img.largeImageURL)}
-            >
-                <img 
-                className={style.ImageGalleryItemImage} 
-                src={img.webformatURL}
-                alt={img.tags}
-                width="320"
-                height="180"
-                />
-            </li>
-        </div>
+    return (        
+        <li 
+        className={style.ImageGalleryItem}
+        key={id}
+        onClick={()=> show(img.largeImageURL)}
+        >
+            <img 
+            className={style.ImageGalleryItemImage} 
+            src={img.webformatURL}
+            alt={img.tags}
+            width="320"
+            height="180"
+            />
+        </li>
     )
 }
 
